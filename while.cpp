bc65bfbfd5e2f4126827b4a9e5faa8312bed06a4
Nefta2134 <colochochristopher@gmail.com>
2026-05-11 15:05:40 -0600

    3.5

---
+++ while.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int numero;
+    int suma=0;
+cout<<"bienvenido, ingrese porfavor todos los numeros que quiera sumar"<<endl;
+cout<<"ingrese los  numeros que quiera sumar, precione 0 para sumar"<<endl;
+    
+    while(numero|=0){
+
+    cout<<"ingrese un numero" << endl;
+    cin>>numero;
+
+    suma= suma+numero;
+    }
+    cout<<"----------------------------------"<<endl;
+    cout<<"|la suma de todos los numeros es:"<< suma << endl;
+    cout<<"----------------------------------"<<endl;
+
+    
+    return 0;
+
+}
